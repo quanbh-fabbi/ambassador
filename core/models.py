1d55553f301e969842d390c69e76fe8adaf0479e
quanbh-fabbi <quanbh@fabbi.io>
2021-10-13 10:01:21 +0700

    6. Add UserManger

--- core/models.py
+++ core/models.py
@@ -31,6 +31,7 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.is_admin = True
         user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
         return user
 
