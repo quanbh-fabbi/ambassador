a15ce909947f0ef6bd8daf3f5d227051d18aadd9
quanbh-fabbi <quanbh@fabbi.io>
2021-10-13 16:47:28 +0700

    14 add LogoutAPIView

--- common/urls.py
+++ common/urls.py
@@ -5,5 +5,7 @@ from .views import *
 urlpatterns = [
     path('register', RegisterAPIView.as_view()),
     path('login', LoginAPIView.as_view()),
+    path('logout', LogoutAPIView.as_view()),
     path('user', UserAPIView.as_view()),
+
 ]
