c5af18206be3f53cf017d9e3df912277fc102162
quanbh-fabbi <quanbh@fabbi.io>
2021-10-14 11:49:07 +0700

    17 + 18. add populate_ambassadors.py and add Faker library

--- administrator/urls.py
+++ administrator/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import AmbassadorAPIView
 
 urlpatterns = [
     path('', include('common.urls')),
+    path('ambassadors', AmbassadorAPIView.as_view()),
 ]
--- administrator/views.py
+++ administrator/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from common.serializers import UserSerializer
+from core.models import *
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class AmbassadorAPIView(APIView):
+    def get(self, _):
+        ambassador = User.objects.filter(is_ambassador=True)
+        serializer = UserSerializer(ambassador, many=True)
+        return Response(serializer.data)
